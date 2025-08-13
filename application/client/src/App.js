0bbd71c7d330150016078904dbc16a27db279cf0
iamrahul59 <rahulbabu2079@gmail.com>
2025-08-13 09:14:58 +0545

    frontend changed

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -52,6 +52,7 @@ function App() {
   return (
     <div className="App">
       <div className="information">
+          <label>Rahul's Own Frontend</label>
           <label>Name:</label>
         <input
           type="text"
